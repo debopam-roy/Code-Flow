7a5aa62d4a38af61689954303efdfc524ed81d1c
DEBOPAM ROY <mapobed@DEBOPAMs-MacBook-Air.local>
2023-11-22 01:26:38 +0530

    UUID generation added

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='/edit/:roomId' element={<EditPage />} />
+          <Route path='/editor/:roomId' element={<EditPage />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,58 @@
-import '../App.css'
-import React from 'react';
-import logo from '../logo.png'
+import '../App.css';
+import React, { useState } from 'react';
+import logo from '../logo.png';
 import { Link } from 'react-router-dom';
+import { v4 as uuid } from 'uuid';
+
 const Home = () => {
+  const [roomId, setRoomId] = useState('');
+  const [username, setUsername] = useState('');
+
+  const createNewRoom = () => {
+    const id = uuid();
+    setRoomId(id);
+    
+  };
+
+  const handleJoinClick = () => {
+    console.log("Hiii");
+  };
+
   return (
-    <div className='pageContainer'>
-      <div className='formContainer'>
-        <img className='logo' src={logo}></img>
-        <span>Enter invitational ROOM ID</span>
-        <input type="text" id="textInput" className='input' placeholder='Enter Room Id' />
-        <input type="text" id="textInput" className='input' placeholder='Enter Username' />
-        <button className='btn'>Join</button>
-        <small className='invitation'>
-          Don't have an invitation code? &nbsp;
-          <Link className='link' to="/create-room">Create room</Link>.
-        </small>
+    <>
+      <div className='pageContainer'>
+        <div className='formContainer'>
+          <img className='logo' src={logo} alt='Code Flow logo' />
+          <span>Enter invitational ROOM ID</span>
+          <input
+            type="text"
+            id="roomIdInput"
+            className='input'
+            placeholder='Enter Room Id'
+            value={roomId}
+            onChange={(e) => { setRoomId(e.target.value) }}
+          />
+          <input
+            type="text"
+            id="usernameInput"
+            className='input'
+            placeholder='Enter Username'
+            value={username}
+            onChange={(e) => { setUsername(e.target.value) }}
+          />
+          <button className='btn' onClick={handleJoinClick}>
+            Join
+          </button>
+          <small className='invitation'>
+            Don't have an invitation code? &nbsp;
+            <Link onClick={createNewRoom} className='link'>
+              Create room
+            </Link>
+          </small>
+        </div>
       </div>
-    </div>
+    </>
   );
-}
+};
 
 export default Home;
