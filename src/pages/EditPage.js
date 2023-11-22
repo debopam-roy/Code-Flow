9329ebb9c03e0b9c86e6909ef5692627557477a8
debopam-roy <debopamroy.cs@gm.rkmvu.ac.in>
2023-11-22 16:48:24 +0530

    React avatar added

--- src/pages/EditPage.js
+++ src/pages/EditPage.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import logo from '../logo.png'
+import Client from '../components/Client';
 
 const EditPage = () => {
+  const [clients, setClients]=useState([
+    {socketId:1, username: "Debopam Roy"},
+    {socketId:1, username: "Arnab Adak"},
+    {socketId:1, username: "Puja Maity"}
+  ]);
   return (
-    <div>EditPage</div>
+    <>
+      <div className='editContainer'>
+        <div className='leftContainer'>
+          <div className='leftInner'>
+            <div className='logoContainer'>
+              <img src={logo} className='left_logo' alt='Code-Flow logo'></img>
+            </div>
+            <h3>Connected</h3>
+            <div className='clientslist'>
+                {
+                  clients.map((client)=><Client username={client.username}/>)
+                }
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
 import '../App.css';
 import React, { useState } from 'react';
 import logo from '../logo.png';
-import { Link } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import { v4 as uuid } from 'uuid';
 
 const Home = () => {
+  const navigate = useNavigate(); 
   const [roomId, setRoomId] = useState('');
   const [username, setUsername] = useState('');
 
@@ -15,29 +16,42 @@ const Home = () => {
   };
 
   const handleJoinClick = () => {
-    console.log("Hiii");
+    if(!roomId || !username)
+      return;
+    navigate(`/editor/${roomId}`,{state:{username}})
   };
+
+  const handleKeyUpEvent =(e) => {
+    if(e.code ==='Enter') {
+      handleJoinClick();
+    }
+  }
 
   return (
     <>
       <div className='pageContainer'>
         <div className='formContainer'>
           <img className='logo' src={logo} alt='Code Flow logo' />
-          <span>Enter invitational ROOM ID</span>
+          <span className='logo-banner'>Enter invitational ROOM ID</span>
           <input
             type="text"
             id="roomIdInput"
             className='input'
+            autoComplete='off'
             placeholder='Enter Room Id'
             value={roomId}
+            onKeyUp={handleKeyUpEvent} 
             onChange={(e) => { setRoomId(e.target.value) }}
           />
+
           <input
             type="text"
             id="usernameInput"
             className='input'
+            autoComplete='off'
             placeholder='Enter Username'
             value={username}
+            onKeyUp={handleKeyUpEvent} 
             onChange={(e) => { setUsername(e.target.value) }}
           />
           <button className='btn' onClick={handleJoinClick}>
