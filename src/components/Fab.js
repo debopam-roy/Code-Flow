b037625c290f154745499084490c53e2a6dc6dd0
DEBOPAM ROY <mapobed@DEBOPAMs-MacBook-Air.local>
2023-11-30 01:15:57 +0530

    All the fab added

--- src/components/Fab.js
+++ src/components/Fab.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import { BsGearWideConnected } from "react-icons/bs";
-
+import theme from '../assets/theme.png';
+import fontSize from '../assets/fontsize.png';
+import settings from '../assets/settings.png';
+import chat from '../assets/chat.png';
 const Fab = () => {
-  return (
-    <div >
-      <div>
 
+
+  return (
+    <div className="fab-main">
+      <div className="fab-buttons">
+        <img src={theme} alt='Theme Icon'/>
       </div>
-      <div className="fab-main">
-        <BsGearWideConnected  size={30} />
+      <div className="fab-buttons">
+        <img src={fontSize} alt='Font Size Icon'/>
+      </div>
+      <div className="fab-buttons">
+        <img src={settings} alt='Settings Icon'/>
+      </div>
+      <div className="fab-buttons">
+        <img src={chat} alt='Chat Icon'/>
       </div>
     </div>
   );
